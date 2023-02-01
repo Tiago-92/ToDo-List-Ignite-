bb6e5949f74a04d55ea764a49f22d2a3c7068ec3
tiago <tiagoamaralcbei@hotmail.com>
2023-01-31 21:02:54 -0300

    aplicado estilos globais e iniciado o HTML e CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import styles from './App.module.css';
+import { PlusCircle } from 'phosphor-react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Logo from './assets/Logo.svg';
 
+export function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main className={styles.body}>
+      <header className={styles.top}>
+        <img src={Logo} alt=""/>
+      </header>
+
+      <div className={styles.task}>
+        <input className={styles.addTask} placeholder="Adicione uma nova tarefa"/>
+        <button className={styles.addButton}>
+          Criar
+          <PlusCircle size={18}/>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </main>
   )
 }
-
-export default App
