342747dd4971233bd1d3855386090fd71d2e8e9f
tiago <tiagoamaralcbei@hotmail.com>
2023-02-01 11:41:19 -0300

    add 2 sessões de divs para informar as tarefas completas e tarefas concluídas

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,20 @@ export function App() {
           <PlusCircle size={18}/>
         </button>
       </div>
+
+      <div className={styles.content}>
+        <div className={styles.status}>
+          <div className={styles.createdTask}>
+            <span>Tarefas Criadas</span>
+            <strong>5</strong>   
+          </div>
+
+          <div className={styles.completedTask}>
+            <span>Concluídas</span>
+            <strong>2 de 5</strong>
+          </div> 
+        </div>
+      </div>
     </main>
   )
 }
